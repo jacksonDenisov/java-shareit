b676b3f85873cb41417f8a7bb419c48886be0176
jacksonDenisov <jackson.denisov@yandex.ru>
2023-02-01 17:28:47 +0300

    feat: приложение разделено на server и gateway

--- ShareItServer/src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
+++ ShareItServer/src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
